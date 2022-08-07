73932b96d3d8dd3c2ac12e312f1e8c172e88aa68
Tony Tamplin <github@tony.dev>
2022-08-07 18:26:50 -0400

    Improved accessibility (according to Lighthouse)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,10 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
-                />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
             <Component {...pageProps} />
         </>
